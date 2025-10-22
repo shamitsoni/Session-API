3a99015d1fabbff1166b702f0321b8390b918e44
Shamit Soni <shamit.soni4@gmail.com>
2025-10-22 18:38:52 -0400

    feat: Utilize Redis for storage
    - Store key-value pairs in Redis instead of dictionary
    - [Temporarily] Use Redis as the main database for data persistence

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
+from redis import Redis
 
 app = Flask(__name__)
-
-dict = {}
+r = Redis(host="localhost", port=6379, db=0)
 
 @app.route("/")
 def home():
@@ -10,15 +10,25 @@ def home():
 
 @app.route("/get", methods=["GET"])
 def get():
-    return jsonify(dict)
+    keys = r.keys("*")
+    result = {}
+    for key in keys:
+        key_str = key.decode("utf-8")
+        val = r.get(key)
+        if val is not None:
+            val_str = val.decode("utf-8")
+        else:
+            val_str = None
+        result[key_str] = val_str
+    return jsonify(result)
 
 @app.route("/set", methods=["POST"])
 def set():
     data = request.get_json()
     key = data.get("key")
     val = data.get("val")
-    dict[key] = val
-    return jsonify(message=f"COMPLETE: {key} set to {val}")
+    r.set(key, val)
+    return jsonify(message=f"SET {key} to {val}")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
