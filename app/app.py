97fa53726b5566e1648d7d2afffd3a660509ef3d
Shamit Soni <shamit.soni4@gmail.com>
2025-10-22 15:16:47 -0400

    feat: Add get and set methods
    Implement simple get and set methods for updating and accessing values
    from temporary storage

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+dict = {}
 
 @app.route("/")
 def home():
     return jsonify(message="Hello from root")
+
+@app.route("/get", methods=["GET"])
+def get():
+    return jsonify(dict)
+
+@app.route("/set", methods=["POST"])
+def set():
+    data = request.get_json()
+    key = data.get("key")
+    val = data.get("val")
+    dict[key] = val
+    return jsonify(message=f"COMPLETE: {key} set to {val}")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
