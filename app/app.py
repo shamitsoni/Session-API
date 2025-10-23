dffa5027bd29a7772eca89794a3d32194b791530
Shamit Soni <shamit.soni4@gmail.com>
2025-10-23 18:23:47 -0400

    feat: Use MongoDB as the primary database
    For persistent data storage, use MongoDB as the main database, instead of Redis

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request
 from redis import Redis
+from pymongo import MongoClient
 
 app = Flask(__name__)
 r = Redis(host="localhost", port=6379, db=0)
+mongo = MongoClient("mongodb://localhost:27017/")
+db = mongo["FlaskApp"]
+collection = db["Values"]
 
 @app.route("/")
 def home():
@@ -10,16 +14,11 @@ def home():
 
 @app.route("/get", methods=["GET"])
 def get():
-    keys = r.keys("*")
-    result = {}
-    for key in keys:
-        key_str = key.decode("utf-8")
-        val = r.get(key)
-        if val is not None:
-            val_str = val.decode("utf-8")
-        else:
-            val_str = None
-        result[key_str] = val_str
+    data = collection.find({})
+    result = []
+    for doc in data:
+        doc.pop("_id", None)
+        result.append(doc)
     return jsonify(result)
 
 @app.route("/set", methods=["POST"])
@@ -27,7 +26,7 @@ def set():
     data = request.get_json()
     key = data.get("key")
     val = data.get("val")
-    r.set(key, val)
+    collection.update_one({"key":key}, {"$set": {"val": val}}, upsert=True)
     return jsonify(message=f"SET {key} to {val}")
 
 if __name__ == "__main__":
