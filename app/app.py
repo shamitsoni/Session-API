9941aad9336308b41286dfd51b0577b4aca1377a
Shamit Soni <shamit.soni4@gmail.com>
2025-10-23 21:33:44 -0400

    feat: Implement Redis for caching
    - Store recently accessed pairs in Redis cache
    - On GET request check the cache first before fetching from primary
      database

--- app/app.py
+++ app/app.py
@@ -12,6 +12,24 @@ collection = db["Values"]
 def home():
     return jsonify(message="Hello from root")
 
+# Retrieve a specific pair
+@app.route("/get/<key>", methods=["GET"])
+def get_key(key):
+    # Check Redis cache first
+    val = r.get(key)
+    if val is not None:
+        return jsonify({key: val.decode("utf-8"), "source": "redis"})
+    
+    # Not in the cache --> retrieve from MongoDB
+    document = collection.find_one({"key": key})
+    if document:
+        val = document["val"]
+        r.set(key, val)
+        return jsonify({key: val, "source": "mongodb"})
+    
+    return jsonify(error="Key not found"), 404
+
+# Retrieve all pairs
 @app.route("/get", methods=["GET"])
 def get():
     data = collection.find({})
@@ -21,12 +39,18 @@ def get():
         result.append(doc)
     return jsonify(result)
 
+# Set a pair
 @app.route("/set", methods=["POST"])
 def set():
     data = request.get_json()
     key = data.get("key")
     val = data.get("val")
+
+    # Store in MongoDB
     collection.update_one({"key":key}, {"$set": {"val": val}}, upsert=True)
+
+    # Store in Redis
+    r.set(key, val)
     return jsonify(message=f"SET {key} to {val}")
 
 if __name__ == "__main__":
