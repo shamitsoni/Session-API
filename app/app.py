02c61f5ec27856557f2ca1757a341a6d98d7b0ab
Shamit Soni <shamit.soni4@gmail.com>
2025-10-31 21:24:48 -0400

    feat: Add sliding expiration for session in persistent storage
    - Differentiate TTL for caching versus storage
    	- Ex: Redis: 30s, MongoDB: 60s
    - Reset expiration time for sessions in peresistent storage upon access,
      as similarly implemented for caching

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ db = mongo["FlaskApp"]
 collection = db["Values"]
 
 CACHE_TTL = 30
+DB_TTL = 60
 
 @app.route("/")
 def home():
@@ -32,7 +33,7 @@ def create_session():
     session_id = str(uuid.uuid4())
 
     # Store in MongoDB
-    expires_at = datetime.now() + timedelta(seconds=CACHE_TTL)
+    expires_at = datetime.now() + timedelta(seconds=DB_TTL)
     collection.update_one(
         {"session_id": session_id},
         {"$set": {
@@ -53,14 +54,26 @@ def get_session(session_id):
     # Check Redis cache first
     session_data = r.get(session_id)
     if session_data is not None:
+        # Reset the expiration time in Redis and MongoDB
         r.expire(session_id, CACHE_TTL)
+        expires_at = datetime.now() + timedelta(seconds=DB_TTL)
+        collection.update_one(
+            {"session_id": session_id}, 
+            {"$set": {"expires_at": expires_at}}
+        )
         return jsonify(session_id=session_id, session_data=session_data.decode("utf-8"), source="redis")
     
     # Not in the cache --> retrieve from MongoDB
     document = collection.find_one({"session_id": session_id})
     if document:
         session_data = document["session_data"]
+        # Add to Redis cache and reset expiration time in MongoDB
         r.setex(session_id, CACHE_TTL, str(session_data))
+        expires_at = datetime.now() + timedelta(seconds=DB_TTL)
+        collection.update_one(
+            {"session_id": session_id}, 
+            {"$set": {"expires_at": expires_at}}
+        )
         return jsonify(session_id=session_id, session_data=session_data, source="mongodb")
 
     return jsonify(error="Session not found"), 404
